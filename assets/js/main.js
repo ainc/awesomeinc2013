72310c05c292340308b076067bf09d7ceeac39db
Roodey <joey8williams@gmail.com>
2015-09-03 10:38:50 -0400

    Team Alpha Services Page

--- assets/js/main.js
+++ assets/js/main.js
@@ -166,3 +166,28 @@ var getWindowWidth = function() {
 
 
 
+$('#myCarousel').carousel({
+  interval: 10000
+});
+
+$('.carousel .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+
+  if (next.next().length>0) {
+ 
+      next.next().children(':first-child').clone().appendTo($(this)).addClass('rightest');
+      
+  }
+  else {
+      $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+     
+  }
+
+ 
+
+
+});
